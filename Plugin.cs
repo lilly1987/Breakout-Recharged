e03a74711fb7d14ddaedf7ca9d06557612faf207
lilly <lilly1987@naver.com>
2022-02-12 01:47:59 +0900

    게임 버그 수정

--- Plugin.cs
+++ Plugin.cs
@@ -15,6 +15,7 @@ namespace MyFirstPlugin
         private static ConfigEntry<bool> localPosition;
         private static ConfigEntry<bool> localPositionLog;
         private static ConfigEntry<bool> loseHealth;
+        private static ConfigEntry<bool> powerupTimer;
         private static ConfigEntry<bool> powerupTimerMax;
         private static ConfigEntry<bool> applyDamage;
         private static ConfigEntry<bool> enemy_Bullet_Update;
@@ -31,6 +32,7 @@ namespace MyFirstPlugin
             loseHealth = Config.Bind("Gameplay", "loseHealth", true);
             localPosition=Config.Bind("Player_Ball", "localPosition", true);
             localPositionLog = Config.Bind("Player_Ball", "log", true);
+            powerupTimer = Config.Bind("Player01", "powerupTimer", true);
             powerupTimerMax = Config.Bind("Player01", "powerupTimerMax", true);
             applyDamage = Config.Bind("BrickTrap", "applyDamage", true);
             enemy_Bullet_Update = Config.Bind("enemy_Bullet", "Update", true);
@@ -64,6 +66,8 @@ namespace MyFirstPlugin
             return false;
         }
 
+        // Gameplay
+
         /// <summary>
         /// succ
         /// </summary>
@@ -92,20 +96,8 @@ namespace MyFirstPlugin
             log.LogInfo($"Enemy_Bullet.Update {v}");
             v.y = -4f;
             __instance.transform.localPosition = v;
-        }        
-        
-        [HarmonyPatch(typeof(Player01), "powerupTimerMax",MethodType.Setter)]
-        [HarmonyPrefix]
-        public static void powerupTimerMaxPre(Player01 __instance, float __0)
-        {
-            if (!powerupTimerMax.Value)
-                return;
-
-            log.LogInfo($"Player01.powerupTimerMax {__0}");
-            __0 *= 100;
-            __instance.powerupTimer= __0;
         }
-        
+
         //[HarmonyPatch(typeof(BrickTurret), "ApplyDamage")]
         //[HarmonyPatch(typeof(BrickTrap), "ApplyDamage")]
         //[HarmonyPrefix]
@@ -117,19 +109,21 @@ namespace MyFirstPlugin
         //    log.LogInfo($"BrickTrap.ApplyDamage ");
         //    return false;
         //}
-        
+
+        #region Player_Ball
+
         /// <summary>
-        /// succ???
+        /// succ??? fail
         /// </summary>
         /// <param name="__instance"></param>
         /// <param name="__result"></param>
         /// <returns></returns>
-        [HarmonyPatch(typeof(Player_Ball), "showLine",MethodType.Getter)]
+        [HarmonyPatch(typeof(Player_Ball), "showLine", MethodType.Getter)]
         [HarmonyPrefix]
-        public static bool showLine(Player_Ball __instance,ref bool __result)
+        public static bool showLine(Player_Ball __instance, ref bool __result)
         {
             log.LogInfo($"Player_Ball.showLine {__result}");
-            __result=true;
+            __result = true;
             __instance.showLine = true;
             return false;
         }
@@ -144,13 +138,13 @@ namespace MyFirstPlugin
         {
             log.LogInfo($"Player_Ball.Start");
             __instance.transform.localScale.Set(2, 2, 2);// not run
-            UnhollowerBaseLib.Il2CppReferenceArray<GameObject> g =GameObject.FindGameObjectsWithTag("Player_Ball");
+            UnhollowerBaseLib.Il2CppReferenceArray<GameObject> g = GameObject.FindGameObjectsWithTag("Player_Ball");
             if (g != null)
             {
                 log.LogInfo($"Player_Ball.Start {g.Count}");
                 foreach (GameObject item in g)
                 {
-                    item.transform.localScale*=2;
+                    item.transform.localScale *= 2;
                 }
             }
         }
@@ -164,19 +158,70 @@ namespace MyFirstPlugin
         public static void Player_Ball_Update(Player_Ball __instance)
         {
             var v = __instance.transform.localPosition;
-            if (localPositionLog.Value)            
+
+            if (v.x < -10 || v.x > 10)
+            {
+                v.x = 0;
+                __instance.transform.localPosition = v;
+            }
+
+            if (localPositionLog.Value)
                 log.LogInfo($"Player_Ball.Update {v}");
-            
-            if (!localPosition.Value)            
+
+            if (!localPosition.Value)
                 return;
-            
+
             // -3.3 3.7
-            if (v.y<=-3.4)
+            if (v.y <= -3.4)
             {
                 v.y = 3.7F;
-                __instance.transform.localPosition=v;
+                __instance.transform.localPosition = v;
             }
         }
+        
+        [HarmonyPatch(typeof(Player_Ball), "hasOrbiting",MethodType.Setter)]
+        [HarmonyPostfix]
+        public static void Player_Ball_hasOrbitingSetter(Player_Ball __instance, bool __0)
+        {
+            log.LogInfo($"Player_Ball.hasOrbiting Setter {__0} , {__instance.maxHits}");
+        }
+
+        [HarmonyPatch(typeof(Player_Ball), "hasOrbiting",MethodType.Getter)]
+        [HarmonyPostfix]
+        public static void Player_Ball_hasOrbitingGetter(Player_Ball __instance, bool __result)
+        {
+            log.LogInfo($"Player_Ball.hasOrbiting Getter {__result}, {__instance.maxHits}");
+        }
+
+        [HarmonyPatch(typeof(Player_Ball), "ClearOrbitingBullets")]
+        [HarmonyPostfix]
+        public static void Player_Ball_ClearOrbitingBullets(Player_Ball __instance)
+        {
+            log.LogInfo($"Player_Ball.ClearOrbitingBullets {__instance.maxHits}");
+        }
+
+        [HarmonyPatch(typeof(Player_Ball), "SetupOrbitingBullets")]
+        [HarmonyPostfix]
+        public static void Player_Ball_SetupOrbitingBullets(Player_Ball __instance)
+        {
+            log.LogInfo($"Player_Ball.SetupOrbitingBullets {__instance.maxHits}");
+        }
+
+        [HarmonyPatch(typeof(Player_Ball), "isNotMultiBallClone", MethodType.Setter)]
+        [HarmonyPostfix]
+        public static void Player_Ball_isNotMultiBallCloneSetter(Player_Ball __instance, bool __0)
+        {
+            log.LogInfo($"Player_Ball.isNotMultiBallClone Setter {__0} , {__instance.maxHits}");
+        }
+
+        [HarmonyPatch(typeof(Player_Ball), "isNotMultiBallClone", MethodType.Getter)]
+        [HarmonyPostfix]
+        public static void Player_Ball_isNotMultiBallCloneGetter(Player_Ball __instance, bool __result)
+        {
+            log.LogInfo($"Player_Ball.isNotMultiBallClone Getter {__result}, {__instance.maxHits}");
+        }
+
+        #endregion
 
         /// <summary>
         /// fail
@@ -200,5 +245,49 @@ namespace MyFirstPlugin
                 }
             }
         }
+
+        #region Player01
+
+
+        [HarmonyPatch(typeof(Player01), "ProccessPickup")]
+        [HarmonyPostfix]
+        public static void ProccessPickup(Player01 __instance)
+        {
+            log.LogInfo($"Player01.ProccessPickup {__instance.powerupTimer} , {__instance.powerupTimerMax}");
+            if (powerupTimerMax.Value)
+                __instance.powerupTimerMax *= 100;
+                
+        } 
+        
+
+        //[HarmonyPatch(typeof(Player01), "powerupTimer", MethodType.Setter)]
+        //[HarmonyPrefix]
+        public static void powerupTimerPre(Player01 __instance, float __0)
+        {
+            if (!powerupTimer.Value)
+                return;
+
+            log.LogInfo($"Player01.powerupTimer {__0}");            
+            __instance.powerupTimer = 0;
+        }
+
+        /// <summary>
+        /// succ??
+        /// </summary>
+        /// <param name="__instance"></param>
+        /// <param name="__0"></param>
+        //[HarmonyPatch(typeof(Player01), "powerupTimerMax", MethodType.Setter)]
+        //[HarmonyPrefix]
+        public static void powerupTimerMaxPre(Player01 __instance, float __0)
+        {
+            if (!powerupTimerMax.Value)
+                return;
+
+            log.LogInfo($"Player01.powerupTimerMax {__0}");
+            __0 *= 100;
+            __instance.powerupTimerMax = __0;            
+        }
+
+        #endregion
     }
 }
